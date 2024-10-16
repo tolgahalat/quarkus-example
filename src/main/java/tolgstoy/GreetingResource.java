88f2d454a3f118df3d49ce41c057a10b198ae931
Tolga Halat <gtolgahalat@gmail.com>
2024-10-17 01:38:56 +0300

    swagger-ui added

--- src/main/java/tolgstoy/GreetingResource.java
+++ src/main/java/tolgstoy/GreetingResource.java
@@ -4,13 +4,38 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Path("/hello")
+@RestController
+//@Path("/hello")
+@RequestMapping("/greeting")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
+//    @GET
+//    @Produces(MediaType.TEXT_PLAIN)
+    @GetMapping
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+    @GetMapping("/{name}")
+    public Greeting hello(@PathVariable(name = "name") String name) {
+        return new Greeting("hello " + name);
+    }
+
+    public class Greeting {
+        private final String message;
+
+        public Greeting(String message) {
+            this.message = message;
+        }
+
+        public String getMessage(){
+            return message;
+        }
+    }
+
 }
